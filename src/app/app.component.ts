59ccf38bb08117a3288cb5cc5fab4b82bee2f82b
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-11-25 20:04:35 +0530

    form design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formValidate';
+  userDetails: FormGroup;
+  constructor(){
+    this.userDetails= new FormGroup({
+      'title': new FormControl('selected', [Validators.required]),
+      'firstName': new FormControl('')
+})
+  }
+
+  onSubmit(){
+    console.log(this.userDetails)
+  }
+
+  get title1(){
+    return this.userDetails.get('title')
+  }
+  get firstname(){
+    return this.userDetails.get('firstname')
+  }
 }
