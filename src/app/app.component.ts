41e9e65434ada4dde07b29b91c243757a2309973
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-11-27 19:49:40 +0530

    till

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'formValidate';
+  submitted = false;
   userDetails: FormGroup;
   constructor(){
     this.userDetails= new FormGroup({
@@ -14,27 +15,16 @@ export class AppComponent {
       'firstName': new FormControl('',[Validators.required, Validators.minLength(3), Validators.pattern('[a-zA-Z]+$')]),
       'lastName': new FormControl('', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-Z]{1,}$')]),
       'userName': new FormControl('', [Validators.required, Validators.pattern('^([A-Z]|[a-z])+([a-z]|[0-9])+$')]),
-      'newPassword': new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern('.*(?=[a-z])')])
+      'newPassword': new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!*%^()])[A-Za-z\d$@$!*%^()]{8,16}$/)])
     })
   }
 
   onSubmit(){
+    this.submitted = true
     console.log(this.userDetails)
   }
 
-  get title1(){
-    return this.userDetails.get('title')
-  }
-  get firstname(){
-    return this.userDetails.get('firstName')
-  }
-  get lastname(){
-    return this.userDetails.get('lastName')
-  }
-  get username(){
-    return this.userDetails.get('userName')
-  }
-  get newPassword(){
-    return this.userDetails.get('newPassword')
+  get f(){
+    return this.userDetails.controls
   }
 }
