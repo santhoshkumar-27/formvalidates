3c640f7e6bcf0be97d41e0803f8b60764199c569
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-12-02 19:27:03 +0530

    add custom validators

--- src/app/mustmatch.validators.ts
+++ src/app/mustmatch.validators.ts
@@ -4,14 +4,12 @@ export function MustMatch(controlName : string, controlNameMatching : string){
    return (fb:FormGroup) => {
        const control = fb.controls[controlName]
        const controlMatching = fb.controls[controlNameMatching]
-
        // This if statement user to return the if any validators is available
        // controlMatching.errors.mustMatch is not defined till now so the (controlMatching.erros.mustMatch) it gives undefined
        // we use to Not operators to convert the undefined to True so that satisfy the this if condition
         if(controlMatching.errors && !controlMatching.errors.mustMatch){
             return;
         }
-
         // we use setErrors method to set manul errors
        if(control.value !== controlMatching.value){
            controlMatching.setErrors({mustMatch: true})
