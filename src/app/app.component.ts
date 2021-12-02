fd1ccbfd82be15dd08a18cc7097d8865382dca6e
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-12-02 19:25:21 +0530

    till to add mobile number and datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MustMatch } from './mustmatch.validators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'formValidate';
   submitted = false;
-  userDetails: FormGroup;
-  constructor(){
-    this.userDetails= new FormGroup({
-      'title': new FormControl('', [Validators.required]),
-      'firstName': new FormControl('',[Validators.required, Validators.minLength(3), Validators.pattern('[a-zA-Z]+$')]),
-      'lastName': new FormControl('', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-Z]{1,}$')]),
-      'userName': new FormControl('', [Validators.required, Validators.pattern('^([A-Z]|[a-z])+([a-z]|[0-9])+$')]),
-      'newPassword': new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!*%^()])[A-Za-z\d$@$!*%^()]{8,16}$/)])
+  userDetails!: FormGroup;
+  constructor(private fb: FormBuilder){
+  }
+  ngOnInit(): void {
+    this.userDetails= this.fb.group({
+      title: ['', [Validators.required]],
+      firstName: ['', [Validators.required, Validators.pattern('[A-Za-z]+$')]],
+      lastName: ['', [Validators.required, Validators.pattern('[A-Za-z]+$')]],
+      userName: ['', [Validators.required, Validators.pattern('^[A-Z]+[a-z]+[0-9]+$')]],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      gender: ['', [Validators.required]],
+      subscription : ['', [Validators.required]]
+    },
+    {
+      validator: MustMatch('newPassword', 'confirmPassword')
     })
   }
-
   onSubmit(){
     this.submitted = true
-    console.log(this.userDetails)
   }
 
   get f(){
